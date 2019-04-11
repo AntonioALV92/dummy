0d81e0d5d9af5a4d318d5bedb0283015cebba7d1
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2019-04-11 13:42:43 -0500

    se agrega el token en el header de los servicios dummy v3

--- services/session/login/login.js
+++ services/session/login/login.js
@@ -1,14 +1,14 @@
-const broker = require('./responseBroker.json');
-const inq = require('./responseInquilino.json');
+// const broker = require('./responseBroker.json');
+// const inq = require('./responseInquilino.json');
 module.exports = (req, res) => {
-    const email = req.body.username;
+    // const email = req.body.username;
 
-    const data = email == 'broker@strat.net' ? broker : inq;
+    // const data = email == 'broker@strat.net' ? broker : inq;
 
     let token = "Bearer eyJhbGciOiJIUzUxMiJ9.";
     token += "89D8duaBMvsg5X6e8YsyPrz_N-C9bDhT0vKxGGG0uKOyexGPAqh0QlKJ7YyIefVVeUfcTpqgNX3gAh4IPX207g";
 
     res.set('Authorization', token);
     res.set('Access-Control-Expose-Headers', 'Authorization, Date');
-    res.status(200).send(data);
+    res.status(200).send({});
 }
